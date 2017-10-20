420cf1e2ba61b3676702b829ee7bdd088f48f83b
Anne Remacle <anne.m.remacle@gmail.com>
2017-10-20 15:38:10 +0200

    intro section on homepage

--- dev/wp-content/themes/saint-leonart/index.php
+++ dev/wp-content/themes/saint-leonart/index.php
@@ -3,3 +3,15 @@
  * Template Name: Homepage
  */
 get_header();?>
+
+<section class="section intro">
+    <h2 class="section__title"><?php the_title(); ?></h2>
+    <div class="section__text">
+        <?php the_content(); ?>
+    </div>
+</section>
+
+<section class="section artistes">
+    <h2 class="section__title">Quelques artistes</h2>
+    
+</section>
