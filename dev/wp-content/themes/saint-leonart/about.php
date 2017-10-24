a7ca33097c5b756034f286ff16f92ba9186b360d
Anne Remacle <anne.m.remacle@gmail.com>
2017-10-24 12:19:50 +0200

    About page intro

---
+++ dev/wp-content/themes/saint-leonart/about.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: À propos
+ */
+get_header();?>
+
+<section class="section">
+    <h2 class="section__title"><?php the_title(); ?></h2>
+    <div class="section__text">
+        <?php the_content(); ?>
+    </div>
+</section>
