e5c17d2165a62a3adf87815f8ac6b65cb575a479
Anne Remacle <anne.m.remacle@gmail.com>
2017-10-24 12:20:45 +0200

    About page team section

--- dev/wp-content/themes/saint-leonart/about.php
+++ dev/wp-content/themes/saint-leonart/about.php
@@ -23,3 +23,21 @@ get_header();?>
     </div>
 </section>
 
+<section class="section team">
+    <h2 class="section__title">L'équipe</h2>
+    <div class="section__text"><?php the_field('intro_equipe'); ?></div>
+
+    <?php
+    if( have_rows('membres') ):
+        while ( have_rows('membres') ) : the_row();?>
+            <section class="member">
+                <figure class="member__figure">
+                    <?php $image = get_sub_field( 'photo' ); ?>
+                    <img class="member__image" src="<?php echo $image['url']; ?>" alt="Portrait de <?php the_sub_field('nom'); ?>" />
+                </figure>
+                <h3 class="member__name"><?php the_sub_field( 'nom' ); ?> </h3>
+                <p class="member__job"><?php the_sub_field( 'fonction' ); ?> </p>
+            </section>
+        <?php endwhile; ?>
+    <?php endif; ?>
+</section>
