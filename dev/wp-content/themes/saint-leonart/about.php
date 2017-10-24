c782963300ad28a952acd1e7afbc5afee0aedcb5
Anne Remacle <anne.m.remacle@gmail.com>
2017-10-24 12:20:08 +0200

    About page newsletter section

--- dev/wp-content/themes/saint-leonart/about.php
+++ dev/wp-content/themes/saint-leonart/about.php
@@ -10,3 +10,16 @@ get_header();?>
         <?php the_content(); ?>
     </div>
 </section>
+
+<section class="section-image newsletter">
+        <?php $image = get_field( 'about_image', 'options' ); ?>
+    <figure class="section-image__figure">
+        <img src="<?php echo $image['url']; ?>" alt="<?php $image['alt'] ?>" class="section-image__image" />
+    </figure>
+    <div class="section-image__content">
+        <h2 class="section__title"><?php the_field( 'about_titre', 'options' ); ?></h2>
+        <div class="section__text"><?php the_field( 'about_texte', 'options' ); ?></div>
+        <!-- TODO: add newsletter form -->
+    </div>
+</section>
+
