bd93e4db943697368219865471ab28c23fa41464
Anne Remacle <anne.m.remacle@gmail.com>
2017-10-24 15:58:41 +0200

    Function to make sections appear & disappear

--- dev/wp-content/themes/saint-leonart/build/js/script.js
+++ dev/wp-content/themes/saint-leonart/build/js/script.js
@@ -13,4 +13,14 @@ $(document).ready(function() {
           }
     } )
 
+    $(".timeline__dates--edition").on( "click", function(e) {
+        e.preventDefault();
+        $(".timeline__dates--edition").removeClass("active");
+        $(this).addClass("active");
+
+        var buttonEdition = $(this).data("edition");
+        $(".timeline__infos").fadeOut("slow");
+        $(".timeline__infos[data-edition="+buttonEdition+"]").fadeIn("slow");
+    } )
+
 });
