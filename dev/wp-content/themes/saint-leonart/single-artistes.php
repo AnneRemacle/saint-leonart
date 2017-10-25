19205fe3462ed42ae19171b579f099851d14905a
Anne Remacle <anne.m.remacle@gmail.com>
2017-10-25 11:50:26 +0200

    intro single artist page

---
+++ dev/wp-content/themes/saint-leonart/single-artistes.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Template Name: Single Artistes
+ */
+get_header();?>
+<?php $terms = get_the_terms( $post->ID, "art-type" ); ?>
+<section class="section artiste">
+    <h2 class="artiste__name">
+        <svg width="60px" height="68px" class="artiste__name--shape <?php echo $terms[0]->slug; ?>">
+            <path d="M0 0 L60 35 L60 68 L0 31 Z" />
+        </svg>
+        <span><?php the_title(); ?></span>
+    </h2>
+    <p class="artiste__domaine"><?php the_field( 'domaine' ); ?></p>
+    <figure class="artiste__portrait">
+        <?php $image = get_field( 'portrait' ); ?>
+        <img src="<?php echo $image[ 'url' ] ?>" alt=" Portrait de <?php the_title(); ?>" class="artiste__image" />
+    </figure>
+    <div class="section__text">
+        <?php the_field( 'presentation' ); ?>
+    </div>
+
+    <ul class="artiste__coordonnees">
+        <?php if( get_field( 'email' ) != null): ?>
+            <li class="artiste__coordonnees--item email">
+                <a href="mailto:<?php the_field( 'email' ); ?>"><?php the_field( 'email' ); ?></a>
+            </li>
+        <?php endif; ?>
+        <?php if( get_field( 'telephone' ) != null): ?>
+            <li class="artiste__coordonnees--item telephone">
+                <a href="tel:<?php the_field( 'telephone' ); ?>"><?php the_field( 'telephone' ); ?></a>
+            </li>
+        <?php endif; ?>
+        <?php if( get_field( 'site' ) != null): ?>
+            <li class="artiste__coordonnees--item site">
+                <a href="<?php the_field( 'site' ); ?>"><?php the_field( 'site' ); ?></a>
+            </li>
+        <?php endif; ?>
+        <?php if( get_field( 'facebook' ) != null): ?>
+            <li class="artiste__coordonnees--item facebook">
+                <a href="<?php the_field( 'facebook' ); ?>"><?php the_title(); ?></a>
+            </li>
+        <?php endif; ?>
+    </ul>
+</section>
