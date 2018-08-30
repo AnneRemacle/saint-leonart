279764f485dff1bde6eeff8c1a1ad6529d5fa185
Anne Remacle <anne.m.remacle@gmail.com>
2018-08-30 16:46:55 +0200

    Rework on single news

--- dev/wp-content/themes/saint-leonart/single-news.php
+++ dev/wp-content/themes/saint-leonart/single-news.php
@@ -6,13 +6,14 @@ get_header();?>
 
 <section class="section">
     <h2 class="section__title"><?php the_title(); ?></h2>
-    <figure class="section__figure">
-        <?php the_post_thumbnail(); ?>
-    </figure>
-    <div class="section__text">
-        <?php the_content(); ?>
+    <div class="single-news-container">
+        <figure class="section__figure">
+            <?php the_post_thumbnail(); ?>
+        </figure>
+        <div class="section__text">
+            <?php the_content(); ?>
+        </div>
     </div>
-
 </section>
 
 <?php get_footer();
